cad29b1e6e0a7835a0070c33606e6c885189fb5e
Arthur Goldberg <artgoldberg@gmail.com>
2017-11-09 11:16:02 -0500

    Add general-purpose, recursive ancestors

--- examples/Week 6- Object-oriented design/person.py
+++ examples/Week 6- Object-oriented design/person.py
@@ -67,25 +67,64 @@ class Person(object):
     def add_child(self, person):
         self.children.append(person)
 
+    def ancestors(self, min_depth, max_depth=None):
+        # collect ancestors whose depth satisfies min_depth <= depth <= max_depth. Thus,
+        # a person's parents and grandparents would have min_depth = 1 and max_depth = 2
+        # returns a set
+        if max_depth is not None:
+            if max_depth < min_depth:
+                    raise ValueError("max_depth ({}) cannot be less than min_depth ({})".format(
+                        max_depth, min_depth))
+        else:
+            # collect just one depth
+            max_depth = min_depth
+        collected_ancestors = set()
+        return self._ancestors(collected_ancestors, min_depth, max_depth)
+
+    def _ancestors(self, collected_ancestors, min_depth, max_depth):
+        # private, recursive ancestors method
+        if min_depth <= 0:
+            collected_ancestors.add(self)
+        if 0 < max_depth:
+            for parent in [self.mother, self.father]:
+                if parent is not None:
+                    parent._ancestors(collected_ancestors, min_depth-1, max_depth-1)
+        return collected_ancestors
+
+    def grandparents_simple(self):
+        return self.ancestors(2)
+
+    def parents_simple(self):
+        return self.ancestors(1)
+
+def print_people(people):
+    for p in people:
+        print('\t', p)
+
 joe = Person('Joe', 'male')
+assert joe.name == 'Joe'
 joe.DOB = '2000-10-12'
 
-print(joe)
-assert joe.name == 'Joe'
-
+print('Joe self'); print_people(joe.ancestors(0))
+print('Joe parents None'); print_people(joe.ancestors(1))
 joe.mother = Person('Mary', 'F')
-print(joe.mother)
-print(joe)
+print('Joe parents mother'); print_people(joe.ancestors(1))
 
-joe.mother.mother = Person('Agnes', 'female')
-print(joe.grandparents())
+maternal_gm = joe.mother.mother = Person('Agnes', 'female')
 joe.father = Person('Joe Sr.', 'male')
+print('joe parents_simple(): Mary, Joe Sr.'); print_people(joe.parents_simple())
 joe.father.father = Person('Old Joe', 'male')
-print(joe.grandparents())
-print('joe.grandparents():')
-for gp in joe.grandparents():
-    print('\t', gp)
+maternal_gm.mother = Person('Maternal ggm', 'female')
+print('joe.grandparents(), Agnes, Old Joe'); print_people(joe.grandparents())
+print('joe.grandparents_simple(), Agnes, Old Joe'); print_people(joe.grandparents_simple())
+print('joe great-grandparents: Maternal ggm'); print_people(joe.ancestors(3))
+
+# play with infinite limits
+inf = float('inf')
+print('joes ancestors: Mary, Joe Sr., Agnes, Old Joe, Maternal ggm'); print_people(joe.ancestors(1, inf))
+print('joe and his ancestors'); print_people(joe.ancestors(0, inf))
 
 joe.mother.add_child(joe)
-print('joe.mother.sons():', joe.mother.sons())
+print('joe.mother.sons():'); print_people(joe.mother.sons())
+print('joe.sons()'); print_people(joe.mother.sons())
 print('joe.sons():', joe.sons())
