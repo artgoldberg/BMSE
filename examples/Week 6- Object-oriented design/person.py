4ead59a65a348573be6547ad0f98836e269167fd
Arthur Goldberg <artgoldberg@gmail.com>
2017-11-13 21:07:05 -0500

    prepare for assignment 4. includes descendant code.

--- examples/Week 6- Object-oriented design/person.py
+++ examples/Week 6- Object-oriented design/person.py
@@ -14,11 +14,12 @@ class Person(object):
         gender (:obj:`str`): a person's gender
         mother (:obj:`Person`): a person's mother
         father (:obj:`Person`): a person's father
-        DOB (:obj:`str`): a person's date of birth
         children (:obj:`set` of `Person`): a person's children
     """
+    # TODO: add a data structure containing this person's variants, and methods that add a variant,
+    # and list all variants
 
-    def __init__(self, name, gender, mother=None, father=None, DOB=None):
+    def __init__(self, name, gender, mother=None, father=None):
         """ Create a Person instance
 
         Create a new Person object, AKA a Person instance. This is used by the
@@ -30,16 +31,18 @@ class Person(object):
              gender (:obj:`str`): the person's gender
              mother (:obj:`Person`, optional): the person's mother
              father (:obj:`Person`, optional): the person's father
-             DOB (:obj:`str`, optional): the person's date of birth
         """
-        # TODO: standardize the representations of gender and DOB
+        # TODO: standardize the representations of gender, so that M, m, and male are valid inputs
+        # for male, but only one standard stored constant represents male, and similarly for female.
         self.name = name
         self.gender = gender
         self.mother = mother
         self.father = father
-        self.DOB = DOB
         self.children = set()
 
+    # a method annotated with '@staticmethod' is a 'static method' that does not receive an
+    # implicit first argument. Rather, it is called C.m(), where the class C identifies the
+    # class in which the method is defined.
     @staticmethod
     def get_persons_name(person):
         """ Get a person's name; if the person is not known, return 'NA'
@@ -59,6 +62,19 @@ class Person(object):
         """
         self.children.add(child)
 
+    # TODO: write 'set_father' and 'set_mother' methods that set a person's father or mother,
+    # and adds the person to the father's or mother's children. Replace all statements below
+    # that set a person's father or mother attribute with these methods.
+
+    # TODO: remove the 'add_child' method and replace any statements that use it with a call to
+    # 'set_father' or 'set_mother'
+
+    # TODO: write 'remove_father' and 'remove_mother' methods that removes a person's father or mother,
+    # and removes the person from the father's or mother's children. Test these methods.
+
+    # TODO: create a 'siblings' method that returns a person's siblings, and write a 'half_siblings'
+    # method that returns a person's half-siblings.
+
     def sons(self):
         """ Get this person's sons
 
@@ -74,14 +90,13 @@ class Person(object):
     def __str__(self):
         """ Provide a string representation of this person
         """
-        return "{}: DOB {}; gender {}; mother {}; father {}".format(
+        return "{}: gender {}; mother {}; father {}".format(
             self.name,
-            self.DOB,
             self.gender,
             Person.get_persons_name(self.mother),
             Person.get_persons_name(self.father))
 
-    def grandparents(self):
+    def grandparents_structured(self):
         ''' Provide this person's grandparents
 
         Returns:
@@ -99,6 +114,52 @@ class Person(object):
         else:
             grandparents.extend([None, None])
         return tuple(grandparents)
+
+    # TODO: EXTRA CREDIT: implement this descendants method. The implementation may use a while
+    # loop or be recursive.
+    def descendants(self, min_depth, max_depth=None):
+        """ Return this person's descendants within a generational depth range
+
+        Obtain descendants whose generational depth satisfies `min_depth` <= depth <= `max_depth`.
+        E.g., this person's children would be obtained with `min_depth` = 1, and this person's
+        grandchildren and great-grandchildren would be obtained with `min_depth` = 3 and
+        `max_depth` = 3.
+
+        Args:
+            min_depth (:obj:`int`): the minimum depth of descendants which should be provided;
+                this person's depth is 0, their children's depth is 1, etc.
+            max_depth (:obj:`int`, optional): the minimum depth of descendants which should be
+                provided; if `max_depth` is not provided, then `max_depth` == `min_depth` so that only
+                descendants at depth == `min_depth` will be provided; a `max_depth` of infinity will
+                obtain all descendants at depth >= `min_depth`.
+
+        Returns:
+            :obj:`set` of `Person`: this person's descendants
+
+        Raises:
+            :obj:`ValueError`: if `max_depth` < `min_depth`
+        """
+        pass
+        
+        if max_depth is not None:
+            if max_depth < min_depth:
+                    raise ValueError("max_depth ({}) cannot be less than min_depth ({})".format(
+                        max_depth, min_depth))
+        else:
+            # collect just one depth
+            max_depth = min_depth
+        depth = 0
+        found = set()
+        # remaining contains pairs (person, depth) of descendants that may satisfy the depth criteria
+        remaining = [(self, 0),]
+        while remaining:
+            (person, depth) = remaining.pop()
+            if min_depth <= depth <= max_depth:
+                found.add(person)
+            if depth < max_depth:
+                for child in person.children:
+                    remaining.append((child, depth+1),)
+        return found
 
     def ancestors(self, min_depth, max_depth=None):
         """ Return this person's ancestors within a generational depth range
@@ -147,7 +208,8 @@ class Person(object):
         Raises:
             :obj:`ValueError`: if `max_depth` < `min_depth`
         """
-        # TODO: a cycle in the ancestry graph will create an infinite loop; avoid this problem
+        # TODO: EXTRA CREDIT: a cycle in the ancestry graph will create an infinite loop;
+        # avoid this problem
         if min_depth <= 0:
             collected_ancestors.add(self)
         if 0 < max_depth:
@@ -156,14 +218,6 @@ class Person(object):
                     parent._ancestors(collected_ancestors, min_depth-1, max_depth-1)
         return collected_ancestors
 
-    def grandparents(self):
-        ''' Provide this person's known grandparents, by using ancestors()
-
-        Returns:
-            :obj:`set`: this person's known grandparents
-        '''
-        return self.ancestors(2)
-
     def parents(self):
         ''' Provide this person's parents
 
@@ -171,6 +225,14 @@ class Person(object):
             :obj:`set`: this person's known parents
         '''
         return self.ancestors(1)
+
+    def grandparents(self):
+        ''' Provide this person's known grandparents, by using ancestors()
+
+        Returns:
+            :obj:`set`: this person's known grandparents
+        '''
+        return self.ancestors(2)
 
     def great_grandparents(self):
         return self.ancestors(3)
@@ -199,8 +261,6 @@ def print_people(people):
 joe = Person('Joe', 'male')
 # check joe's name
 assert joe.name == 'Joe'
-# set joe's DOB
-joe.DOB = '2000-10-12'
 
 print('Joe self'); print_people(joe.ancestors(0))
 print('Joe parents None'); print_people(joe.ancestors(1))
@@ -217,6 +277,7 @@ print('joe parents(): Mary, Joe Sr.'); print_people(joe.parents())
 joe.father.father = Person('Old Joe', 'male')
 # create joe's maternal grandmother
 maternal_gm.mother = Person('Maternal ggm', 'female')
+print('joe.grandparents_structured(), Agnes, Old Joe'); print_people(joe.grandparents_structured())
 print('joe.grandparents(), Agnes, Old Joe'); print_people(joe.grandparents())
 print('joe great-grandparents: Maternal ggm'); print_people(joe.great_grandparents())
 
@@ -229,3 +290,9 @@ print('joe and his ancestors'); print_people(joe.ancestors(0, inf))
 joe.mother.add_child(joe)
 print('joe.mother.sons():'); print_people(joe.mother.sons())
 print('joe.sons():', joe.sons())
+
+kid = Person('kid', 'male', father=joe)
+joe.add_child(kid)
+
+print('joe.mother.descendants(2):'); print_people(joe.mother.descendants(2))
+print('joe.mother.descendants(1, max_depth=2):'); print_people(joe.mother.descendants(1, max_depth=2))
