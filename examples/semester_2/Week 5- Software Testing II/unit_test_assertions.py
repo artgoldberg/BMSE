a0bf19e8b7e1e00f4d7e2e90bc6ce44243b63a0f
Arthur Goldberg <artgoldberg@gmail.com>
2018-01-31 09:02:23 -0500

    make unit test examples

---
+++ examples/semester_2/Week 5- Software Testing II/unit_test_assertions.py
@@ -0,0 +1,52 @@
+"""
+:Author: Arthur Goldberg <Arthur.Goldberg@mssm.edu>
+:Date: 2018-01-30
+:Copyright: 2018, Arthur Goldberg
+"""
+import unittest
+
+
+class TestStringMethods(unittest.TestCase):
+
+    # Try many of the most useful unit test assertions
+    # See them at https://docs.python.org/3/library/unittest.html#assert-methods
+
+    def test_demo_some_assert_methods(self):
+        l = [3, 2, 8, 9]
+        # l == l
+        self.assertEqual(l, l)
+        m = [3, 2, 8, 9]
+        m.reverse()
+        # l != m
+        self.assertNotEqual(l, m)
+        m.reverse()
+        # l == m
+        self.assertEqual(l, m)
+        self.assertTrue(l == m)
+        self.assertFalse(l is m)
+
+        # 3 in l
+        self.assertIn(3, l)
+        # -2 not in l
+        self.assertNotIn(-2, l)
+
+        # test exception, in a context manager
+        with self.assertRaises(IndexError):
+            l[len(l)]
+
+        # test exception, and check error message
+        with self.assertRaises(ValueError) as context:
+            raise ValueError("It cannot be")
+        self.assertIn('cannot', str(context.exception))
+
+        a = 7
+        b = a - 1E-9
+        # round(a-b, 7) == 0
+        self.assertAlmostEqual(a, b)
+
+        # re.search(s)
+        data_dependent_string = "Today {} is greater than {}".format(a, b)
+        self.assertRegex(data_dependent_string, "Today .* is greater than")
+        
+if __name__ == '__main__':
+    unittest.main()
---
+++ examples/semester_2/Week 5- Software Testing II/unit_test_examples.py
@@ -0,0 +1,28 @@
+"""
+:Author: Arthur Goldberg <Arthur.Goldberg@mssm.edu>
+:Date: 2018-01-30
+:Copyright: 2018, Arthur Goldberg
+:License: MIT
+"""
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+class TestStringMethodsException(unittest.TestCase):
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
