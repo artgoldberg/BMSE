2fc2f922a6f6577d89799d60f675316e3a3f1dc0
Arthur Goldberg <artgoldberg@gmail.com>
2018-09-12 16:26:30 -0400

    revise chap 2 syntax

---
+++ python-for-developers/Chapter2_Syntax/example.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# the line above is a functional comment
+
+# the 'pragma no cover' comment tells 
+if False:   # pragma no cover
+    print('Surprise!')
+else:
+    print('else clause')
