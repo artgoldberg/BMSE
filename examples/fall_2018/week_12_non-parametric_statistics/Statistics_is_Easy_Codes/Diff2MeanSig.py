863f219f83d08ebc427e5ad6a80e1f4a157ddc73
Arthur Goldberg <artgoldberg@gmail.com>
2020-03-26 13:21:09 -0400

    df

--- examples/fall_2018/week_12_non-parametric_statistics/Statistics_is_Easy_Codes/Diff2MeanSig.py
+++ examples/fall_2018/week_12_non-parametric_statistics/Statistics_is_Easy_Codes/Diff2MeanSig.py
@@ -3,11 +3,9 @@
 # Difference between Two Means Significance Test
 # From: Statistics is Easy! By Dennis Shasha and Manda Wilson
 # 
-# Assuming that there is no significant difference in the means of 
-# the two samples, tests to see the probability of getting a difference 
-# greater than or equal to the observed difference in the means by chance 
-# alone.  Uses shuffling & bootstrapping to get a distribution to compare 
-# to the observed statistic.
+# Assuming that there is no significant difference in the means of two samples, tests to see the probability
+# of getting a difference greater than or equal to the observed difference in the means by chance alone.  
+# Uses shuffling & bootstrapping to get a distribution to compare to the observed statistic.
 # 
 # Author: Manda Wilson
 #
@@ -19,25 +17,23 @@
 #
 # Pseudocode:
 #
-# 1. Measure the difference between the two group means.  The difference in means is measured
-#    by (sum(grpA) / len(grpA)) - (sum(grpB) / len(grpB)).  In this example the difference between
+# 1. Measure the difference between the two group means.  The difference in means is
+#    (sum(grpA) / len(grpA)) - (sum(grpB) / len(grpB)).  In this example the difference between
 #    the two group means is 12.97.
 #
-# 2. Set a counter to 0, this will count the number of times we get a difference
-#    between the means greater than or equal to 12.97.  
+# 2. Shuffle the group labels and count the times the shuffled means are greater than or equal to the original difference.  
 #
 # 3. Do the following 10,000 times:
 #    a. Shuffle the original measurements.  To do this:
-#       i. put the values from all the groups into one array but remembering the start and end indexes of each group
+#       i. put the values from all the groups into one array, remembering the start and end indexes of each group
 #       ii. shuffle the values in the array, effectively reassigning the values to different groups
-#    b. Measure the difference between the two group means, just as we did in step (1).
-#    c. If the difference from step (3b) is greater than or equal to 12.97, increment our counter 
-#       from step (2). Note: if our original difference between the means were a negative value 
-#       we would check for values less than or equal to that value.
+#    b. Measure the difference between the two group means, using step (1).
+#    c. If the difference from step (3b) is greater than or equal to the original difference, increment a counter.
+#       Note: if our original difference between the means were negative, check for values less than or equal to that value.
 #
 # 4. counter / 10,000 equals the probability of getting our observed difference of two means greater than
 #    or equal to 12.97, if there is in fact no significant difference.
-#
+
 
 import random
 
@@ -47,21 +43,21 @@ input_file = "stateasyExerciseData/Diff2MeanCh1Ex2.vals"
 input_file = "Diff2Mean.vals"
 print('input', input_file)
 
-# Subroutines
-
 # takes a list of groups (two or more)
 # pools all values, shuffles them, and makes new groups of same size as original groups
 # returns these new groups
-# example of shuffle with more than two groups: OneWayAnovaSig.py
 def shuffle(grps):
+    # grps is a list of lists
 	num_grps = len(grps)
-	pool = []
 	
 	# pool all values
+	pool = []
 	for i in range(num_grps):
 		pool.extend(grps[i])
+
 	# mix them up
 	random.shuffle(pool)
+
 	# reassign to groups of same size as original groups
 	new_grps = []
 	start_index = 0
