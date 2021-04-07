f93c0a2801a4d888211116a2080c7315314c34b8
Arthur Goldberg <artgoldberg@gmail.com>
2021-04-06 20:20:40 -0400

    little change

--- examples/BMI2002_SP_21/HW7_example/HW7_correction.py
+++ examples/BMI2002_SP_21/HW7_example/HW7_correction.py
@@ -3,7 +3,7 @@ import argparse
 def solve(a, b, c, real_roots=True):
     print(a, b, c, real_roots)
 
-parser = argparse.ArgumentParser(description='Example parsing that solves a mistake in HW 7')
+parser = argparse.ArgumentParser(description='Example arg parsing that solves a mistake in HW 7')
 parser.add_argument('a', type=float, help="coefficient 'a' in a quadratic equation")
 parser.add_argument('b', type=float, help="coefficient 'b' in a quadratic equation")
 parser.add_argument('c', type=float, help="coefficient 'c' in a quadratic equation")
